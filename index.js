cd330aacf5d8eda59d8929f0393719dd11b921cf
andrew <andrew@cloud-latitude.com>
2022-03-21 01:10:10 +0800

    check ban

--- index.js
+++ index.js
@@ -10,31 +10,29 @@ const client = StreamChat.getInstance('92uhdv5t2jke','rrhnsf286ntc5kswmknbvqtpz4
 app.get('/',async (req, res) => {
 
 
-    const {blocklists} = await client.listBlockLists();
-
-    console.log(blocklists[0].words)
-
-    await client.createBlockList({
-        name: 'blocklist',
-        words:[...blocklists[0].words, 'http'],
-    });
-
-
-    // await client.banUser('1379', {
-    //     banned_by_id: '1379', // ID of the user who is performing the ban (Server-side auth)
-    //     timeout: 60,
-    //     reason: 'Banned for one hour',
-    // });
+    // const {blocklists} = await client.listBlockLists();
     //
+    // console.log(blocklists[0].words)
+    //
+    // await client.createBlockList({
+    //     name: 'blocklist',
+    //     words:[...blocklists[0].words, 'http'],
+    // });
+
+
+    await client.banUser('1379', {
+        banned_by_id: '1379', // ID of the user who is performing the ban (Server-side auth)
+        timeout: 60,
+        reason: 'Banned for one hour',
+    });
 
     // console.log(client)
     //
-    // await client.unbanUser('1378');
-    //
-    //
-    // const response = await client.queryBannedUsers({channel_cid: 'commerce:public'});
-    //
-    // console.log(response)
+    // await client.unbanUser('1379');
+
+    const response = await client.queryBannedUsers({channel_cid: 'commerce:public'});
+
+    console.log(response)
 
     res.send(response);
 
